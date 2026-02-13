580688178ac069316458fcee7cb7d3048af763d7
Sreeja170 <sreejagodugu18@gmail.com>
2026-02-13 12:24:22 +0530

    message

---
+++ IVth-sem/Python/Practice/M02_Logic_building_&_Patterns/S03/PS04_Number_validation.py
@@ -0,0 +1,18 @@
+'''
+n = int(input())
+l = len(str(n))
+res = 0
+for i in str(n):
+    res += int(i) ** l
+if n == res:
+    print("Armstrong")
+else:
+    print("not Armstrong")
+'''
+#Perfect number
+n = int(input())
+s = 0
+for i in range(1, n//2 + 1):
+    if n % i == 0:
+        s += i
+print("perfect" if n == s else "not perfect")
