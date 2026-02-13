15b77340a3acb60341fc7157d66298339c2ee8ea
Sreeja170 <sreejagodugu18@gmail.com>
2026-02-13 12:35:06 +0530

    mes1

--- IVth-sem/Python/Practice/M02_Logic_building_&_Patterns/S03/PS04_Number_validation.py
+++ IVth-sem/Python/Practice/M02_Logic_building_&_Patterns/S03/PS04_Number_validation.py
@@ -8,7 +8,7 @@ if n == res:
     print("Armstrong")
 else:
     print("not Armstrong")
-'''
+
 #Perfect number
 n = int(input())
 s = 0
@@ -16,3 +16,8 @@ for i in range(1, n//2 + 1):
     if n % i == 0:
         s += i
 print("perfect" if n == s else "not perfect")
+'''
+def factorial(n):
+    if n < 0:
+        
+
